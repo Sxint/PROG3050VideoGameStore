d26bb481652826a40cb1e18c55d00fddb1d89e0b
Sxint <saintsolider79@gmail.com>
2023-09-15 17:38:56 -0400

    Add project files.

---
+++ PROG3050VideoGameStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG3050VideoGameStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
