7cf98e9bce188512251a3450b4fdb8ca3f379a6d
ethan <ethan01tai@gmail.com>
2023-09-21 14:46:35 -0400

    added Register and Login pages with basic styling

---
+++ PROG3050VideoGameStore/Controllers/LoginRegisterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROG3050VideoGameStore.Models;
+using System.Diagnostics;
+
+namespace PROG3050VideoGameStore.Controllers
+{
+    public class LoginRegisterController : Controller
+    {
+        private readonly ILogger<LoginRegisterController> _logger;
+
+        public LoginRegisterController(ILogger<LoginRegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
