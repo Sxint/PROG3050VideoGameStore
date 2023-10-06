b589fddc9373ec650c7ec3d57ab90c266d50320e
ethan <ethan01tai@gmail.com>
2023-10-06 11:20:16 -0400

    added admin panel view and stylesheet

---
+++ PROG3050VideoGameStore/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG3050VideoGameStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Panel()
+        {
+            return View();
+        }
+    }
+}
