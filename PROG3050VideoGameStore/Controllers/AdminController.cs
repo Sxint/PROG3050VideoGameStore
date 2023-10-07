aa73a1b1d7557512cb29e4d807df70670000b22a
ethan <ethan01tai@gmail.com>
2023-10-07 18:06:35 -0400

    Revert "Added edit and view details pages for admin panel"
    This reverts commit 9a4d4c251c1cb810f16d0fc075179135f3a39096.

--- PROG3050VideoGameStore/Controllers/AdminController.cs
+++ PROG3050VideoGameStore/Controllers/AdminController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using PROG3050VideoGameStore.Models;
 
 namespace PROG3050VideoGameStore.Controllers
 {
@@ -9,26 +7,6 @@ namespace PROG3050VideoGameStore.Controllers
         public IActionResult Panel()
         {
             return View();
-        }
-
-        [HttpGet] // GET request to display the edit game form
-        public IActionResult EditGame()
-        {
-            // Logic to display the edit game form
-            return View();
-        }
-
-        [HttpPost] // POST request to handle form submission
-        public IActionResult EditGame(Game model)
-        {
-            // Logic to handle the form submission and update the game
-            return RedirectToAction("Panel"); // Redirect to the desired action
-        }
-
-        public IActionResult ViewDetails()
-        {
-            // Logic to display the edit game form
-            return View("../Games/GameDetails");
         }
     }
 }
