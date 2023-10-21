10abbc77939ade361fcf414c4eb3627eb328cb0e
Ethan Tai <ethan01tai@gmail.com>
2023-10-21 14:58:58 -0400

    Worked on home pages, added home page for after user logs in, worked on profile page, fixed efit form margins and footer being weird

--- PROG3050VideoGameStore/Controllers/ProfileController.cs
+++ PROG3050VideoGameStore/Controllers/ProfileController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+using PROG3050VideoGameStore.Models;
 
 namespace PROG3050VideoGameStore.Controllers
 {
     public class Profile : Controller
     {
-        public IActionResult profile()
+        public Profile(AppDbContext appDbContext)
         {
-            return View();
+            _appDbContext = appDbContext;
         }
+        public IActionResult profile(int id)
+        {
+            //dynamic models: https://www.c-sharpcorner.com/UploadFile/ff2f08/multiple-models-in-single-view-in-mvc/
+            dynamic user = new ExpandoObject();
+            user.Profile = _appDbContext.Profiles.Find(id);
+            user.Preferences = _appDbContext.ProfilePreferencesList.Find(id);
+            return View(user);
+        }
+
+        private AppDbContext _appDbContext;
+
     }
 }
