8edd787de3b8d5bd342a790c59ffa544b70b0932
dnirmal2163 <dnirmal2163@conestogac.on.ca>
2023-12-01 21:53:35 -0500

    ProfilePageComplete

--- PROG3050VideoGameStore/Controllers/ProfileController.cs
+++ PROG3050VideoGameStore/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Dynamic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PROG3050VideoGameStore.Models;
 using PROG3050VideoGameStore.ViewModels;
 
@@ -11,13 +12,31 @@ namespace PROG3050VideoGameStore.Controllers
         {
             _appDbContext = appDbContext;
         }
-        public IActionResult profile(int id)
+        public IActionResult profile(int id=0)
         {
-            //dynamic models: https://www.c-sharpcorner.com/UploadFile/ff2f08/multiple-models-in-single-view-in-mvc/
-            dynamic user = new ExpandoObject();
-            user.Profile = _appDbContext.Profiles.Find(id);
-            user.Preferences = _appDbContext.ProfilePreferencesList.Find(id);
-            return View(user);
+            ProfileVM model = new ProfileVM();
+            model.ProfileId = id;
+            model.Preference = _appDbContext.ProfilePreferencesList.Include(p => p.Profile).FirstOrDefault(p => p.UserProfileId == id);
+            model.AllFriendsList = _appDbContext.FamilyFriendsList.Where(f => (f.UserProfileId == id || f.ReceivedBy == id) && f.Status == "Accepted").ToList();
+            model.AllProfiles = model.AllProfiles = _appDbContext.Profiles.ToList();
+            model.AllGames = new List<Game>();
+            List<Order> orderList = _appDbContext.Orders.Include(o => o.OrderItems).Where(o => o.UserProfileId == id).ToList();
+
+            foreach (Order order in orderList)
+            {
+                if (order.OrderStatus == "Confirmed")
+                {
+                    foreach (OrderItem item in order.OrderItems)
+                    {
+                        Game game = _appDbContext.Games.Find(item.GameId);
+                        model.AllGames.Add(game);
+
+                    }
+                }
+
+            }
+
+            return View(model);
         }
 
      
